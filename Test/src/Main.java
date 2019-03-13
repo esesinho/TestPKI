d69b94519fe27d2b5ca483fbd0fc42525b8a46ee
Szymon Sobieszczyk <esesinho@gmail.com>
2019-03-13 13:41:10 +0100

    Commit b1

---
+++ Test/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    static int policz(int a, int b){
+        return a+b;
+    }
+        public static void main(String[] args){
+            System.out.print(policz(2,3));
+        }
+}
+
